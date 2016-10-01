1d8609815b2d4a6ee0e0c6cb7676c71c6441ad53
sungjine456 <sungjine456@gmail.com>
2016-10-01 23:18:51 +0900

    계단 오르기 2579

---
+++ OnlineJudgeProblem/Problem2579/Problem2579.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2579 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] ia = new int[n + 1];
+        int[] maxia = new int[n + 1];
+        for(int i = 0; i < n; i++){
+        	ia[i] = Integer.parseInt(br.readLine());
+        }
+    	maxia[0] = ia[0];
+    	if(n == 1){
+    		System.out.println(maxia[0]);
+    		return;
+    	}
+    	maxia[1] = ia[0] + ia[1];
+    	if(n == 2){
+    		System.out.println(maxia[1]);
+    		return;
+    	}
+    	maxia[2] = max(ia[0] + ia[2], ia[1] + ia[2]);
+        for(int i = 3; i < n; i++){
+        	maxia[i] = max(ia[i] + maxia[i - 2], ia[i] + ia[i - 1] + maxia[i - 3]);
+        }
+        System.out.println(maxia[n - 1]);
+	}
+	static int max(int a, int b){
+		return a > b ? a : b;
+	}
+}
