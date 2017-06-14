583cc0dc3c0ea1d8521f9c28498f557ed803dfd0
sungjine456 <sungjine456@gmail.com>
2017-06-14 19:08:27 +0900

    퀵 소트 내용 수정

--- QuickSort.java
+++ QuickSort.java
@@ -1,54 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Random;
-import java.util.StringTokenizer;
-
 public class QuickSort {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		StringTokenizer token = new StringTokenizer(str, " ");
-		int size = Integer.parseInt(token.nextToken());
-		int num = Integer.parseInt(token.nextToken());
-		Integer[] array = new Integer[size];
-		str = br.readLine();
-		token = new StringTokenizer(str, " ");
-		for(int i = 0; i < size; i++){
-			array[i] = Integer.parseInt(token.nextToken());
+	static int[] arr = {7,5,3,2,1,6,4};
+	static int t;
+	public static void main(String[] args) {
+		quickSort(0, arr.length-1);
+		for(int i : arr){
+			System.out.print(i + " ");
 		}
-		System.out.println(quickSort(array, num - 1));
 	}
 	
-	private static Integer quickSort(Integer[] array, int k){
-		int l = 0;
-		int r = array.length - 1;
-		Random ran = new Random();
-		while(r >= l){
-			int pivot = ran.nextInt(r - l + 1) + l;
-			int pivotVal = array[pivot];
-			swap(array, pivot, r);
-			int pivotIndex = l;
-			for(int i = l; i < r; i++){
-				if(array[i] < pivotVal){
-					swap(array, i, pivotIndex);
-					pivotIndex++;
-				}
-			}
-			swap(array, r, pivotIndex);
-			if(pivotIndex == k){
-				return array[pivotIndex];
-			} else if(pivotIndex < k){
-				l = pivotIndex + 1;
+	private static void quickSort(int left, int right){
+		int pivot = arr[right];
+		int l = left;
+		int r = right-1;
+		while(l <= r){
+			int lv = arr[l];
+			int rv = arr[r];
+			if(lv >= pivot && rv <= pivot){
+				swap(l++, r--);
+			} else if(lv < pivot && rv < pivot){
+				l++;
+			} else if(lv > pivot && rv > pivot){
+				r--;
 			} else {
-				r = pivotIndex - 1;
+				l++;
+				r--;
 			}
 		}
-		return null;
+		if(pivot < arr[l]){
+			swap(l, right);
+		}
+		if(left<l-1)quickSort(left, l-1);
+		if(l+1<right)quickSort(l+1, right);
 	}
 	
-	private static void swap(Integer[] array, int index1, int index2){
-		int temp = array[index1];
-		array[index1] = array[index2];
-		array[index2] = temp;
+	private static void swap(int index1, int index2){
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
 	}
 }
